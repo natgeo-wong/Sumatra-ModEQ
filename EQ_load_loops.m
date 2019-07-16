fdbb84780b8b4f7d962143a63d0696a9f4b019f5
Nathanael Wong <natgeo.wong@outlook.com>
2019-07-16 19:18:16 +0800

    Added comments to loop files

--- EQ_load_loops.m
+++ EQ_load_loops.m
@@ -1,54 +1,32 @@
 function [ size,pos ] = EQ_load_loops (lon,lat,xyz)
 
-%                          EQ_loopsize_para2.m
-%    EQ Function that defines Range for Loop Variables for Para 2 Loops
+%                             EQ_load_loops.m
+%      EQ Function that creates the positions of the fine-grid loop
 %                   Nathanael Wong Zhixin, Feng Lujia
 %
-% This function calculates the range for variables that are being looped in
-% para 2 loops, or position loops, based upon the earthquake epicenter
-% longitude and latitude (flttypes 1 & 3), or top-left fault-rupture
-% longitude and latitude (flttypes 2 & 4).
+% This function creates the high-resolution (lon,lat) grid about which the
+% event is looped through based on the initial best-fit model given by the
+% initial coarse-resolution loop.
 %
 % INPUT:
-% -- EQlon    : Longitude of Epicenter (?) (flttypes 1 & 3)
-%               Longitude of top-left Fault-Rupture (?) (flttypes 2 & 4)
-% -- EQlat    : Latitude of Epicenter (?) (flttypes 1 & 3)
-%               Latitude of top-left Fault-Rupture (?) (flttypes 2 & 4)
-% -- rake     : Rake of the fault-displacement
-% -- loopsize : range of values which the loop covers.
+% -- lon : longitude of best-fit start-point (from coarse grid)
+% -- lat : latitude of best-fit start-point (from coarse grid)
+% -- xyz : earthquake initial input coordinates
 %
 % OUTPUT:
-% -- rakemin  : minimum rake analysed
-% -- rakemax  : maximum rake analysed
-% -- lonmin   : minimum longitude analysed (?)
-% -- lonmax   : maximum longitude analysed (?)
-% -- latmin   : minimum latitude analysed (?)
-% -- latmax   : maximum latitude analysed (?)
-% -- lonstep  : increments of increase in longitude (?)
-% -- latstep  : increments of increase in latitude (?)
+% -- size : number of points in grid (i.e. nlon*nlat)
+% -- pos  : array containing 
 %
-% FORMAT OF CALL: EQ_loopsize_para1 (longitude, latitude, rake, loopsize)
+% FORMAT OF CALL: EQ_load_loops (longitude,latitude,event coordinates)
 %
 % OVERVIEW:
-% 1) This function will first use the variable "loopsize" to determine the
-%    size of the loop (ie. the range through which the variables to be
-%    analysed by the loop is run through).
+% 1) This function creates vectors for the possible longitudes and
+%    latitudes from the input (lon,lat) coordinates
 %
-% 2) If the loop is large (ie. loopsize = 1), then the variables rakemin,
-%    lonmin, lonmax, latmin and latmax can be calculated directly
-%    from the input variables EQlon, EQlat and rake.
+% 2) The function then calls the subfunction to create the gridded
+%    coordinate positions and the grid size
 %
-%    However, if the loop is small (ie. loopsize = 2), then the function
-%    will call for an input of the variables cenlon, cenlat and cenrake,
-%    which will define the mean value of the range.  The variables rakemin,
-%    rakemax, lonmin, lonmax, latmin and latmax will be calculated from
-%    cenrake, cenlon and cenlat.
-%
-% 3) The function will then calculate the step for each iteration of the
-%    loop by taking the variables rakemax, rakemin, lonmax, lonmin, latmax
-%    and latmin.
-%
-% 4) The variables will then all be exported to the parent function.
+% 3) The variables will then all be exported to the parent function.
 %
 % VERSIONS:
 % 1) -- Created on 20160613 by Nathanael Wong
@@ -58,15 +36,17 @@ function [ size,pos ] = EQ_load_loops (lon,lat,xyz)
 %       earthquakes where a tiny shift in position results in a large
 %       change in misfit (like in strike-slip earthquakes with a very high
 %       dip and thus location is highly limited)
+%
+% 2) -- Rewritten sometime in 2017
+%
+% 3) -- Final version validated and commented on 20190715 by Nathanael Wong
 
 %%%%%%%%%%%%%%%%%%%%%%%%% DETERMINING LOOP RANGE %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-lonmin  = lon - 0.5;  latmin  = lat - 0.5;
-lonmax  = lon + 0.5;  latmax  = lat + 0.5;
-lonstep = 0.01;       latstep = 0.01;
+lonmin = lon - 0.5; lonmax = lon + 0.5; lonstep = 0.01;
+latmin = lat - 0.5; latmax = lat + 0.5; latstep = 0.01;
 
-lon = lonmin : lonstep : lonmax;
-lat = latmin : latstep : latmax;
+lon = lonmin : lonstep : lonmax; lat = latmin : latstep : latmax;
 pos = [ lon ; lat ];
 
 [ size,pos ] = EQ_load_loopgrid (pos,xyz);
--- EQ_load_slipvec.m
+++ EQ_load_slipvec.m
@@ -1,18 +1,47 @@
-function [ slipvec ] = EQ_load_slipvec (rakeii,data,size)
+function [ slip ] = EQ_load_slipvec (rake,data,size)
 
+%                          EQ_load_slipvec.m
+%      EQ Function that calculates the slip vectors for each rake
+%                   Nathanael Wong Zhixin, Feng Lujia
+%
+% This function calculates the slip vectors for each rake.  The slip along
+% the rake of course remains constant, but the slip along the strike, dip
+% and tensile directions all change depending on the rake angle.
+%
+% INPUT:
+% -- rake : rake for this iteration
+% -- data : event information
+% -- size : grid size
+%
+% OUTPUT:
+% -- slip : array containing the slip vectors (replicated to match the grid
+%           size for calling and printing)
+%
+% FORMAT OF CALL: EQ_load_slipvec (rake,event data,grid size)
+%
+%
+% OVERVIEW:
+% 1) Takes as input the rake of the loop iteration, the earthquake initial
+%    data and the grid size of the loop
+%
+% 2) Calculate the strike-slip from the rake
+%
+% 3) Converts the rake to the equivalent in (0,360), then calculate the
+%    dip-slip and strike-slip
+%
+% 3) Create the slip array.
+%
+% VERSIONS:
+% 1) -- Created sometime in 2017
+%
+% 2) -- Final version validated and commented on 20190715 by Nathanael Wong
 
+rs = data.slip(3); rake = mod(rake,360); ss = rs * cosd(rake);
 
-rs = data.slip(3);
-
-rakeii = mod(rakeii,360);
-ss = rs * cosd(rakeii);
-
-if rakeii >= 0 && rakeii <= 180
-    ds = rs * sind(rakeii); ts = 0;
-else
-    ts = rs * sind(rakeii); ds = 0;
+if rake <= 180, ds = rs * sind(rake); ts = 0;
+else,           ts = rs * sind(rake); ds = 0;
 end
 
-slip = [ rs ss ds ts ]; slipvec = repmat (slip,size,1);
+slip = [ rs ss ds ts ]; slip = repmat(slip,size,1);
 
 end
